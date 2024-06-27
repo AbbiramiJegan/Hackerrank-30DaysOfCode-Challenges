ad81cebcac977bc96f564c526de23b14c1af2218
AbbiramiJegan <108280861+AbbiramiJegan@users.noreply.github.com>
2024-06-28 00:21:05 +0800

    Add function to determine if a number is "Weird" or "Not Weird"

--- conditionalStatements.py
+++ conditionalStatements.py
@@ -1,3 +1,22 @@
+"""
+Determines if a given integer is "Weird" or "Not Weird" based on specific conditions.
+
+- If the integer is odd, print "Weird".
+- If the integer is even and in the inclusive range of 2 to 5, print "Not Weird".
+- If the integer is even and in the inclusive range of 6 to 20, print "Weird".
+- If the integer is even and greater than 20, print "Not Weird".
+
+Args:
+    n (int): A positive integer.
+
+Returns:
+    None
+
+Example:
+    For n = 3, output is "Weird".
+    For n = 24, output is "Not Weird".
+"""
+
 def conditional(n):
     if n % 2 != 0:
         print("Weird")
